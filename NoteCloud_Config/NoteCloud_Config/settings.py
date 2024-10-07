6e56ad9545b785c2b7ce3fb002ea113961109111
Varsto04 <ilya-ovchinnikov-04-04@yande.ru>
2024-10-07 22:03:50 +0300

    Added authentication

--- NoteCloud_Config/NoteCloud_Config/settings.py
+++ NoteCloud_Config/NoteCloud_Config/settings.py
@@ -161,8 +161,8 @@ LOGIN_REDIRECT_URL = '/'
 ACCOUNT_LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = os.environ.get("EMAIL_PORT", 587)
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = int(os.environ.get("EMAIL_USE_TLS", default=1))
