97293bf6a15e3dcd83619182393af93768a9dd1e
cantevenexist <cantevenexist@gmail.com>
2024-10-07 14:09:00 +0300

    base news

--- NoteCloud_Config/news/models.py
+++ NoteCloud_Config/news/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.conf import settings
 
 class News(models.Model):
     title = models.CharField(max_length=200)
@@ -8,6 +7,3 @@ class News(models.Model):
     image = models.ImageField(upload_to='news_images/', null=True, blank=True)  # Новое поле для изображения
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
