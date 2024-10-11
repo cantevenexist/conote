d20d193bea461e2412b908309875d6e4212cbcab
cantevenexist <160029919+cantevenexist@users.noreply.github.com>
2024-10-11 14:37:56 +0300

    url rename

--- NoteCloud_Config/news/urls.py
+++ NoteCloud_Config/news/urls.py
@@ -3,7 +3,7 @@ from .views import NewsListView, NewsDetailView, NewsCreateView, NewsUpdateView,
 
 urlpatterns = [
     path('', NewsListView.as_view(), name='news_list'),
-    path('view/<slug:slug>/', NewsDetailView.as_view(), name='news_detail'),
+    path('detail/<slug:slug>/', NewsDetailView.as_view(), name='news_detail'),
     path('create/', NewsCreateView.as_view(), name='news_create'),
     path('edit/<slug:slug>/', NewsUpdateView.as_view(), name='news_edit'),
     path('delete/<slug:slug>/', NewsDeleteView.as_view(), name='news_delete'),
