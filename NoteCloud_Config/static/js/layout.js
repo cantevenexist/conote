81852930a64973c698e5cb16502cbd73e7e6383a
cantevenexist <160029919+cantevenexist@users.noreply.github.com>
2024-10-28 21:39:33 +0300

    design header + gitignore added media not-include

--- NoteCloud_Config/static/js/layout.js
+++ NoteCloud_Config/static/js/layout.js
@@ -1,20 +1,23 @@
-function getScreenPixelWidth() {
-      const devicePixelRatio = window.devicePixelRatio || 1;
-      const screenWidth = window.innerWidth;
-      const zoomLevel = document.documentElement.style.zoom || 1;
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.button_header img');
+    const logotype = document.querySelectorAll('.logotype img');
+    images.forEach(function(img) {img.setAttribute('draggable', 'false');});
+    logotype.forEach(function(img) {img.setAttribute('draggable', 'false');});
+});
 
-      return screenWidth * devicePixelRatio / zoomLevel;
+function toggleMenu() {
+    var user_settings_panel = document.getElementById("user_settings_panel");
+    var toggle_button = document.getElementById("toggle_button");
+    user_settings_panel.classList.toggle("show");
+    toggle_button.classList.toggle('active');
 }
 
-const layoutCentralBlock = document.querySelector('.layout_central_block');
-layoutCentralBlock.style.maxWidth = getScreenPixelWidth() + 'px';
+window.onclick = function(event) {
+    var user_settings_panel = document.getElementById("user_settings_panel");
+    var toggle_button = document.getElementById("toggle_button");
 
-
-function toggleDiv() {
-        var user_settings_panel = document.getElementById("user_settings_panel");
-        if (user_settings_panel.classList.contains("show")) {
-            user_settings_panel.classList.remove("show");
-        } else {
-            user_settings_panel.classList.add("show");
-        }
+    if (!user_settings_panel.contains(event.target) && !toggle_button.contains(event.target)) {
+        user_settings_panel.classList.remove("show");
+        toggle_button.classList.remove('active');
+    }
 }
